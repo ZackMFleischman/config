6ed42d081330c34230a60064fe037f5a4ac6c5a8
Zack Fleischman <zFleischman@counsyl.com>
2016-01-19 17:21:14 -0800

    Added autoreload to ipython

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -539,3 +539,6 @@ c.TerminalInteractiveShell.confirm_exit = False
 # If True, any %store-d variables will be automatically restored when IPython
 # starts.
 # c.StoreMagics.autorestore = False
+
+c.InteractiveShellApp.extensions = ['autoreload']
+c.InteractiveShellApp.exec_lines = ['%autoreload 2']
